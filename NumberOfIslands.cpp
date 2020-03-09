8801c2057cb6ce7d687c51057da1a49939c050bd
saumz <45761512+saumz@users.noreply.github.com>
2020-03-09 16:42:01 +0530

    Update NumberOfIslands.cpp

--- NumberOfIslands.cpp
+++ NumberOfIslands.cpp
@@ -10,21 +10,21 @@ public:
                 if(grid[i][j]=='1')
                 {
                     count+=1;
-                    callbfs(grid,i,j);
+                    calldfs(grid,i,j);
                 }
             }
         }
         return count;
     }
     
-    int callbfs(vector<vector<char>>& grid,int  i,int j)
+    int calldfs(vector<vector<char>>& grid,int  i,int j)
     {
         if(i<0 || i>grid.size() ||j<0||j>grid[i].size()||grid[i][j]=='0') return 0;
         grid[i][j]='0';
-        callbfs(grid,i-1,j);//up
-        callbfs(grid,i+1,j);//down
-        callbfs(grid,i,j+1);//right
-        callbfs(grid,i,j-1);//left
+        calldfs(grid,i-1,j);//up
+        calldfs(grid,i+1,j);//down
+        calldfs(grid,i,j+1);//right
+        calldfs(grid,i,j-1);//left
         return 0;
     }
 };
